734ac8d2a85fb07a7730899067d08725df28491a
jasonjsimon <103650963+jasonjsimon@users.noreply.github.com>
2025-09-17 14:57:54 -0500

    Update __init__.py

--- components/blackview_lock/__init__.py
+++ components/blackview_lock/__init__.py
@@ -1,21 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import ble_client
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['ble_client']
+# This component no longer depends on ble_client
+DEPENDENCIES = []
 
 blackview_lock_ns = cg.esphome_ns.namespace('blackview_lock')
-BlackviewLock = blackview_lock_ns.class_('BlackviewLock', cg.Component, ble_client.BLEClientNode)
+BlackviewLock = blackview_lock_ns.class_('BlackviewLock', cg.PollingComponent)
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = cv.polling_component_schema('60s').extend({
     cv.GenerateID(): cv.declare_id(BlackviewLock),
-    cv.Required("ble_client_id"): cv.use_id(ble_client.BLEClient),
-}).extend(cv.COMPONENT_SCHEMA)
+})
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    parent = await cg.get_variable(config["ble_client_id"])
-    # Corrected: Using the function name suggested by the compiler
-    cg.add(parent.register_ble_node(var))
